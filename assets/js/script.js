93fb42fd2aadc5f4c6ff7d5a772621b7fb140636
kingayo12 <oreoluwaa08@gmail.com>
2023-07-25 23:10:35 +0100

    okay

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const main = document.querySelector('main');
 const arrow = document.querySelector('i');
 const out= document.querySelector('.out');
 const navCls= document.querySelectorAll('.nav-cl');
+const cnt= document.querySelectorAll('.connect-body');
 collapse.addEventListener('click', () =>{
     aside.classList.toggle('expand')
     arrow.classList.contains('fa-arrow-right') ? arrow.classList.toggle('fa-arrow-left'): arrow.classList.remove('fa-arrow-left')
@@ -23,4 +24,16 @@ main.addEventListener('click', () =>{
     });
 
 })
+
+wcnt.addEventListener('click', () =>{
+    aside.classList.remove('expand')
+    out.classList.remove('in')
+    arrow.classList.remove('fa-arrow-left')
+    navCls.forEach((navCl) => {
+        navCl.classList.remove('in')  
+    });
+
+})
+
+
 
